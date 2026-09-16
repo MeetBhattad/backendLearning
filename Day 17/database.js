e723dbd572cd7a7a670da8434cbf53da296a9857
MeetBhattad <meetbhattad@gmail.com>
2026-09-16 11:46:33 +0530

    Data Sanitization

---
+++ Day 17/database.js
@@ -0,0 +1,10 @@
+import { connect } from 'mongoose';
+
+async function main() {
+   
+  await connect("mongodb+srv://coderArmy9:Hunter%409Bhai@codingadda.4ugikcf.mongodb.net/Instagram");
+  
+}
+
+
+export default main;
